98f9429a00d4b22c66149c50d334af7419fb9acd
AnLau25 <avald024@uottawa.ca>
2023-01-07 19:19:23 -0500

    idk what I changed

--- Resp.js
+++ Resp.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#425263';
+		header.style.backgroundColor = '#60788a';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
